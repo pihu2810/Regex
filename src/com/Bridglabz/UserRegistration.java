acff07335002662702d320e01404510d3f1c6d24
pihu2810 <vishaka.kadam01@gmail.com>
2022-04-11 11:29:40 +0530

    add  UserRegistration First name UC1

--- src/com/Bridglabz/UserRegistration.java
+++ src/com/Bridglabz/UserRegistration.java
@@ -1,10 +1,24 @@
 package com.Bridglabz;
 
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
 public class UserRegistration {
 
-	public static void main(String[] args) {
-		System.out.println("Welcome to the user registration");
+	static Scanner sc = new Scanner(System.in);
 
-	}
+  public static void firstName() {
+	        System.out.println("Enter First Name");
+	        String fn = sc.nextLine();
+	        if (Pattern.matches("^[A-Z]{1}[a-z]{2,}$", fn) == true)
+	            System.out.println("Valid First Name");
+	        else
+	            System.out.println("Invalid First Name");
+	    }
+	    public static void main(String[] args) {
+	        firstName();
+	    }
+
+	
 
 }
