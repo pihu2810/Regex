15cd9fefdb2b75b8d4b5370b1e513d2e0138e595
pihu2810 <vishaka.kadam01@gmail.com>
2022-04-18 15:52:40 +0530

    add J_Unit Validation UC11

--- src/main/java/com/Bridglabz/EmailChecker.java
+++ src/main/java/com/Bridglabz/EmailChecker.java
@@ -3,9 +3,9 @@ package com.Bridglabz;
 import java.util.regex.Pattern;
 
 public class EmailChecker {
-	private static final String EMAIL_PATTERN = "^[a-z0-9]{3,}+([_+-.][a-z0-9]{3,}+)*@[a-z0-9]+.[a-z]{2,3}+(.[a-z]{2,3}){0,1}$";
+	private static final String Email_PATTERN = "^[a-z0-9]{3,}+([_+-.][a-z0-9]{3,}+)*@[a-z0-9]+.[a-z]{2,3}+(.[a-z]{2,3}){0,1}$";
     public boolean validateEmail(String email) {
-        Pattern pattern = Pattern.compile(EMAIL_PATTERN);
-        return pattern.matches(EMAIL_PATTERN,email);
+        Pattern pattern = Pattern.compile(Email_PATTERN);
+        return pattern.matches(Email_PATTERN,email);
     }
 }
